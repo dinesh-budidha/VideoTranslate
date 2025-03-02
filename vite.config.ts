0d832a682b1567fa62e661851ef4725d0fd9ae44
Budidha Dinesh Goud <114913650+dinesh-budidha@users.noreply.github.com>
2025-03-02 10:40:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+export default defineConfig({
+  base: "/your-repo-name/", // Replace with your GitHub repository name
+});
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/dinesh-budidha/VideoTranslate.git",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
